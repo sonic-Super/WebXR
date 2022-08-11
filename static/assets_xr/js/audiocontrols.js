90a9f90081f56400ee596f1530c810d634f6b993
sonic-Super <lonrgenhu@gmail.com>
2022-08-11 14:25:56 -0400

    agghhhh

---
+++ static/assets_xr/js/audiocontrols.js
@@ -0,0 +1,27 @@
+AFRAME.registerComponent('songplayer', {
+
+      init: function () {
+
+        let audiosource = document.querySelector('#musicpanel');
+
+        let musicplay = () => {
+        audiosource.components.sound.playSound()
+        }
+
+        this.el.addEventListener('click', musicplay);
+
+      }});
+
+AFRAME.registerComponent('songstopper', {
+      init: function () {
+
+        let audiosource = document.querySelector('#musicpanel');
+
+        let musicstop = () => {
+        audiosource.components.sound.stopSound()
+        }
+
+        this.el.addEventListener('click', musicstop);
+
+      }});
+
